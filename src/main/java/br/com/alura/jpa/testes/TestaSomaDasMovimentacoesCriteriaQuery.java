f62a7d72df3e015746a7daafc3918d4502f5552c
William Foleis Mian <william_mian@yahoo.com.br>
2020-12-24 10:56:37 -0200

    CriteriaQuery

---
+++ src/main/java/br/com/alura/jpa/testes/TestaSomaDasMovimentacoesCriteriaQuery.java
@@ -0,0 +1,35 @@
+package br.com.alura.jpa.testes;
+
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Root;
+
+import br.com.alura.jpa.modelo.Movimentacao;
+
+public class TestaSomaDasMovimentacoesCriteriaQuery {
+	
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("financas");
+        EntityManager em = emf.createEntityManager();
+        
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery<BigDecimal> query = builder.createQuery(BigDecimal.class);
+        
+        Root<Movimentacao> root = query.from(Movimentacao.class);
+        
+        Expression<BigDecimal> sum = builder.sum(root.<BigDecimal>get("valor"));
+        query.select(sum);
+        
+        TypedQuery<BigDecimal> typedQuery = em.createQuery(query);
+        
+        System.out.println("A soma das movimentações é: " + typedQuery.getSingleResult());
+	}
+
+}
